b9bca27b78166add1f815cf00f55a6ea07ac5203
Simon9791 <Simon9791@Gmail.com>
2025-07-13 07:31:58 +0800

    新增 @EnableDiscoveryClient 註解以啟用服務發現功能

--- src/main/java/com/example/taskapp/TaskApplication.java
+++ src/main/java/com/example/taskapp/TaskApplication.java
@@ -5,8 +5,10 @@ package com.example.taskapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskApplication.class, args);
